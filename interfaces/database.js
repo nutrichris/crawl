7dfd422004b736e829f62d7cd4f6f5e4fe5a1c2f
nutrichris <christian.peter.andres@googlemail.com>
2017-01-05 11:05:47 +0100

    tried to switch to postgres

--- interfaces/database.js
+++ interfaces/database.js
@@ -1,57 +1,21 @@
-var log = require('log4js');
-var logger = log.getLogger('database');
+var log = require('log4js').getLogger('database');
+var process = require('process');
 
-var mongoose = require('mongoose');
-var mongolastic = require('mongolastic');
+var pg = require('pg');
 
 var exports = module.exports = {};
 
-var require = function(path) {
-    return exports;
-};
+var client = null;
 
 exports.init = function(config, next) {
-    var hosts = config.replSet.hosts;
-    var hostString = "";
-
-    for (var i = 0; i < hosts.length; i++) {
-	var h = hosts[i];
-	hostString += h.name + ":" + h.port;
-	if (i < (hosts.length - 1)) {
-	    hostString += ",";
-	}
-    }
-
-    var url = "mongodb://" + hostString + "/" + config.name + '?replicaSet=' + config.replSet.name;
-    logger.info("Connecting: " + url);
-
-    mongoose.connect(url);
-    mongolastic.connect('crawl', {
-	host: "localhost:9200",
-	sniffOnStart: false
-    }, function(err) {
-	if (err) {
-	    logger.error(err);
-	} else {
-	    logger.info("elasitcsearch connect success");
-	}
-    });    
-    var db = mongoose.connection;
-
-    db.on('error', console.error.bind(console, 'connection error: '));
-    db.once('open', function(callback) {
-	logger.info('database connected');
-	if (next !== null && next !== undefined) {
-	    return next(null);
-	}
+    pg.defaults.ssl = true;
+    log.info(process.env);
+    pg.connect(process.env.DATABASE_URL, function(err, c) {
+	client = c;
+	return next(err);
     });
+
 };
 
-exports.clear = function() {
-    var db = mongoose.connection;
-};
 
-exports.close = function() {
-    logger.info('close database');
-    mongoose.connection.close();
-};
+
