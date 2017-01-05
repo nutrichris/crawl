23b640d28d77a33a0fd9839c532d391c9fcfa975
nutrichris <christian.peter.andres@googlemail.com>
2017-01-05 14:41:06 +0100

    saerdh

--- middle/search.js
+++ middle/search.js
@@ -8,7 +8,7 @@ exports.search = function(query, next) {
 	if (!err) {
 	    var result = [];
 	    for (var i = 0; i < data.length; i++) {
-		if (data[i].title.indexof(query) != -1) {
+		if (data[i].title.indexOf(query) != -1) {
 		    result.push(data[i]);
 		}
 	    }
