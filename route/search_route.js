5d0e98f58db6c6e03bae462770c4fd09a7216b9f
nutrichris <christian.peter.andres@googlemail.com>
2017-01-05 14:46:21 +0100

    search

--- route/search_route.js
+++ route/search_route.js
@@ -4,12 +4,19 @@ var search = require('../middle/search.js');
 var exports = module.exports = {};
 
 exports.search = function(req, res, next) {
-    search.search("winter", function(err, result) {
-	if (!err) {
-	    return res.status(200).send(result);
-	} else {
-	    return res.status(500).send(err);
-	}
-    });
+    var query = req.query.q;
+
+    if (query !== undefined && query !== null) {
+	search.search("winter", function(err, result) {
+	    if (!err) {
+		return res.status(200).send(result);
+	    } else {
+		return res.status(500).send(err);
+	    }
+	});
+    } else {
+	return res.status(500).send("Please search for something");
+    }
+
 };
 
