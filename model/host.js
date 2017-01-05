0672f3be40eb8e33eea743f82a5c9081c5941d49
nutrichris <christian.peter.andres@googlemail.com>
2017-01-05 11:41:09 +0100

    first db statement

--- model/host.js
+++ model/host.js
@@ -3,7 +3,17 @@ var log = require('log4js').getLogger('host');
 var exports = module.exports = {};
 
 exports.findOne = function(client, criteria, next) {
-    exports.find(criteria, next);
+    exports.find(client, criteria, function(err, results) {
+	if (!err) {
+	    if (results.length > 0) {
+ 		return next(null, results[0]);
+	    } else {
+		return next(null, null);
+	    }
+	} else {
+	    return next(err);
+	}
+    });
 }
 
 exports.find = function(client, criteria, next) {
@@ -21,7 +31,14 @@ exports.find = function(client, criteria, next) {
 	}
 	command = command.substr(0, command.length - 5) + ";";
 	log.info(command);
-	return next(null, command);
+	const query = client.query(command);
+	var results = [];
+	query.on('row', fucntion(row) {
+	    results.push(row);
+	});
+	query.on('end', function() {
+	    return next(null, results);	    
+	});
     } else {
 	return next("database connection is null");
     }
